249c654e78ba1ed32e0b2224ec02938a85a295a5
Timon Reich <timon.reich@gmx.de>
2023-02-11 12:20:07 +0100

    start new project

--- alvr_linux_x86_64/build_alvr.sh
+++ alvr_linux_x86_64/build_alvr.sh
@@ -1,5 +1,5 @@
 pushd alvr
 cargo xtask clean
-cargo xtask prepare-deps --platform linux --no-nvidia --local-ffmpeg
-cargo xtask build-server --platform linux --no-nvidia --local-ffmpeg
+cargo xtask prepare-deps --platform linux --no-nvidia --gpl
+cargo xtask build-server --platform linux --no-nvidia --gpl
 popd
